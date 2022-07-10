f306d2486b4b3be7c4807522754744b083af4f6e
gajananardalkar <gajananardalkar999@gmail.com>
2022-07-10 20:08:14 +0530

    Promise

--- PromiseProj/app.js
+++ PromiseProj/app.js
@@ -9,12 +9,17 @@
     document.getElementById('btn').addEventListener('click', fetchQuote)
     let authorsContainer = document.getElementById('authorsContainer')
 
-  function fetchQuote(){
+  function fetchQuote(event){
+//Author List
+    let specificAuthor = event.target.innerHTML;
+
     let authorsContainer = document.getElementById('authorsContainer')
     authorsContainer.innerHTML = ''
-    document.getElementById('quote').innerHTML = 'none' //data[randomQuote].text
 
+    document.getElementById('quote').innerHTML = " " //data[randomQuote].text
+    document.getElementById('btn').style.display = 'none'
 
+//Fetching quotes
     fetch("https://type.fit/api/quotes")
     .then(function(response){
         // console.log(response.json())
@@ -24,8 +29,7 @@
         let dataLength = data.length;
         console.log(dataLength)
         let randomQuote = Math.floor(Math.random()*dataLength)
-        document.getElementById('quote').innerHTML = data[randomQuote].text
-
+        // document.getElementById('quote').innerHTML = data[randomQuote].text
         // let authors = []
         
         for(let i = randomQuote ; i <= randomQuote+5 ; i++){
@@ -35,14 +39,28 @@
             if(authorname != null){
                 console.log(authorname)
                 //  authors.push(authorname)
-
                  let authorDiv = document.createElement('div')
                  authorDiv.classList.add('authorDivclass')
                  authorDiv.innerHTML = authorname
-                 authorDiv.addEventListener('click', authorQuote )
+                  authorDiv.addEventListener('click', fetchQuote )
                  authorsContainer.appendChild(authorDiv)
             } 
         }
+
+        let requestedQuote = data.filter((authorquote)=>{
+         return  authorquote.author === specificAuthor
+        })
+        console.log(requestedQuote)
+
+        requestedQuote.map((quote)=>{
+          let requestedQuotesDiv = document.createElement('div');
+          requestedQuotesDiv.classList.add('qcard');
+          let requestedQuotesPara = document.createElement('p');
+          requestedQuotesPara.innerHTML  = quote.text;
+          document.getElementById('quotesContainer').appendChild(requestedQuotesDiv);
+          requestedQuotesDiv.appendChild(requestedQuotesPara);
+  
+        })
             // authors.map((author)=>{
             //     let authorDiv = document.createElement('div')
             //     authorDiv.classList.add('authorDivclass')
@@ -53,10 +71,6 @@
     })
   }
 
-  function authorQuote(event){
-    let specificAuthor = event.target.innerHTML;
-    fetchQuote();
-
-  }
+  
 
   
